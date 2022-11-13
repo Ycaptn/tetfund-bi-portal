2e432327797223070003bb58725191a1e0b168df
apollosgeofrey <apollosgeofrey@gmail.com>
2022-11-13 20:03:38 +0100

    bi  ui improvement

--- resources/views/pages/beneficiaries/show.blade.php
+++ resources/views/pages/beneficiaries/show.blade.php
@@ -1,6 +1,44 @@
 @extends('layouts.app')
 
 @section('app_css')
+    <style type="text/css">
+        /* Style the tab */
+        .tab {
+          overflow: hidden;
+          border: 1px solid #ccc;
+          background-color: #f1f1f1;
+        }
+
+        /* Style the buttons inside the tab */
+        .tab button {
+          background-color: inherit;
+          float: left;
+          border: none;
+          outline: none;
+          cursor: pointer;
+          padding: 14px 16px;
+          transition: 0.3s;
+          font-size: 17px;
+        }
+
+        /* Change background color of buttons on hover */
+        .tab button:hover {
+          background-color: #ddd;
+        }
+
+        /* Create an active/current tablink class */
+        .tab button.active {
+          background-color: #ccc;
+        }
+
+        /* Style the tab content */
+        .tabcontent {
+          display: none;
+          padding: 6px 12px;
+          border: 1px solid #ccc;
+          border-top: none;
+        }
+    </style>
 @stop
 
 @section('title_postfix')
@@ -52,65 +90,54 @@ Beneficiary
                 @include('tf-bi-portal::pages.beneficiaries.show_fields')
             </div>
             <div class="col-lg-12">
-                <br><br><h5 class="text-center pt-2" style="border-top: 1px solid lightgray;"> Beneficiary Members </h5><hr>
-                <div class="table-responsive">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>
-                                    S/N
-                                </th>
-                                <th>
-                                    Full Name
-                                </th>
-                                <th>
-                                    Email
-                                </th>
-                                <th>
-                                    Phone
-                                </th>
-                                <th>
-                                    Roles
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @if(isset($beneficiary_members) && count($beneficiary_members) > 0)
-                                @php
-                                    $counter = 0;
-                                @endphp
-                                @foreach($beneficiary_members as $beneficiary_member)
-                                    <tr>
-                                        <td>
-                                            <b>{{ $counter += 1 }}). </b>  
-                                        </td>
-                                        <td>
-                                            {{ $beneficiary_member->user->fullname }}
-                                        </td>
-                                        <td>
-                                            {{ $beneficiary_member->user->email }}
-                                        </td>
-                                        <td>
-                                            {{ $beneficiary_member->user->telephone }}
-                                        </td>
-                                        <td>
-                                            @php
-                                                $user_roles = $beneficiary_member->user->roles()->pluck('name')->toArray();
-                                            @endphp
-                                            @if(count($user_roles) > 0)
-                                                @foreach($user_roles as $user_role)
-                                                    <b>||</b>
-                                                    {{ucwords($user_role)}}
-                                                    <b>||</b>
-                                                    <br>
-                                                @endforeach
-                                            @endif
-                                        </td>
-                                    </tr> 
-                                @endforeach
-                            @endif
-                        </tbody>
-                    </table>
+                <div class="tab pb-2 mt-3" style="border-top: thin solid lightgray; border-bottom: thin solid lightgray;">
+                    <ul class="nav">
+                        <li class="mt-3" style="margin-right: 3px;">
+                            <a href="#members?members=members" class="tablinks btn btn-primary btn-md shadow-none" onclick="openCity(event,'members')" id="defaultOpen">
+                                Members
+                            </a>                        
+                        </li>
+                        <li class="mt-3" style="margin-right: 3px;">                            
+                            <a href="#submissions?submissions=submissions" class="tablinks btn btn-primary btn-md shadow-none" onclick="openCity(event, 'submissions')">
+                                Submissions
+                            </a>
+                        </li>
+                        <li class="mt-3" style="margin-right: 3px;">                                
+                            <a href="#nominations?nominations=nominations" class="tablinks btn btn-primary btn-md shadow-none" onclick="openCity(event, 'nominations')">
+                                Nominations
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+
+                {{-- sub menu contents --}}
+                <div id="members" class="tabcontent">
+                    <h5 class="pt-2"> 
+                        <strong>
+                            Beneficiary Members 
+                        </strong>
+                    </h5>
+                    @include('tf-bi-portal::pages.beneficiaries.partials.beneficiaryMembers') 
+                </div>
+
+                <div id="submissions" class="tabcontent">
+                    <h5 class="pt-2"> 
+                        <strong>
+                            Submissions Requests
+                        </strong>
+                    </h5>
+                    {{-- include view path --}}
+                </div>
+
+                <div id="nominations" class="tabcontent">
+                    <h5 class="pt-2"> 
+                        <strong>
+                            Nominations 
+                        </strong>
+                    </h5>
+                    {{-- include view path --}}                    
+                </div>   
+                <div class="">
                 </div>
             </div>
         </div>
@@ -131,4 +158,22 @@ Beneficiary
 @stop
 
 @push('page_scripts')
+    <script type="text/javascript">
+        function openCity(evt, cityName) {
+          var i, tabcontent, tablinks;
+          tabcontent = document.getElementsByClassName("tabcontent");
+          for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+          }
+          tablinks = document.getElementsByClassName("tablinks");
+          for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+          }
+          document.getElementById(cityName).style.display = "block";
+          evt.currentTarget.className += " active";
+        }
+
+        // Get the element with id="defaultOpen" and click on it
+        document.getElementById("defaultOpen").click();
+    </script>
 @endpush
