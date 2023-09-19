7c4b07ee65f366fa0dbf1f28e4eb84b0d8a554d5
“maitiigor” <oluwatosinadesoyesalami@gmail.com>
2023-09-19 11:07:05 +0100

    minor update

--- resources/views/pages/nomination_requests/partial_sub_modals/partials_request_nomination/fields.blade.php
+++ resources/views/pages/nomination_requests/partial_sub_modals/partials_request_nomination/fields.blade.php
@@ -2,23 +2,29 @@
     <label class="col-sm-12 control-label" for="nomination_type">Type of Nomination</label>
     <div class="col-sm-12 input-group">
         <select name="nomination_type" id="nomination_type" class="form-select">
-        <option value="">Select type of Nomination</option> 
-            @if(isset($beneficiary->type) && strtolower($beneficiary->type)=='college' && isset($beneficiary_member->member_type) && strtolower($beneficiary_member->member_type)=='academic')
-                <option value="tp">Teaching Practice &nbsp; (TP)</option> 
+            <option value="">Select type of Nomination</option>
+            @if (isset($beneficiary->type) &&
+                    strtolower($beneficiary->type) == 'college' &&
+                    isset($beneficiary_member->member_type) &&
+                    strtolower($beneficiary_member->member_type) == 'academic')
+                <option value="tp">Teaching Practice &nbsp; (TP)</option>
             @endif
-            <option value="ca">Conference Attendance &nbsp; (CA)</option> 
-            @if(isset($beneficiary_member->member_type) && strtolower($beneficiary_member->member_type)=='academic')
-                <option value="tsas">TETFund Scholarship for Academic Staff &nbsp; (TSAS)</option>
-            @endif
+            <option value="ca">Conference Attendance &nbsp; (CA)</option>
+
+            <option value="tsas">TETFund Scholarship for Academic Staff &nbsp; (TSAS)</option>
+
         </select>
         <span class="input-group-text"><span class="fa fa-archive"></span></span>
     </div>
 </div>
 
-@if(isset($beneficiary->type) && strtolower($beneficiary->type)=='college' && isset($beneficiary_member->member_type) && strtolower($beneficiary_member->member_type)=='academic')
+@if (isset($beneficiary->type) &&
+        strtolower($beneficiary->type) == 'college' &&
+        isset($beneficiary_member->member_type) &&
+        strtolower($beneficiary_member->member_type) == 'academic')
     <div class="row col-sm-12 form-group mb-3" id="tp_nomination_form" style="display: none;">
         <hr>
-        @include('pages.t_p_nominations.fields')        
+        @include('pages.t_p_nominations.fields')
     </div>
 @endif
 
@@ -27,9 +33,8 @@
     @include('pages.c_a_nominations.fields')
 </div>
 
-@if(isset($beneficiary_member->member_type) && strtolower($beneficiary_member->member_type)=='academic')
-    <div class="row col-sm-12 form-group mb-3" id="tsas_nomination_form" style="display: none;">
-        <hr>
-        @include('pages.t_s_a_s_nominations.fields')
-    </div>
-@endif
+
+<div class="row col-sm-12 form-group mb-3" id="tsas_nomination_form" style="display: none;">
+    <hr>
+    @include('pages.t_s_a_s_nominations.fields')
+</div>
